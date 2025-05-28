6e3162cf33a3535ff1b03f46d42231b3c2b55f60
zahidbenu <zahidbenu@gmail.com>
2025-05-28 22:44:45 +0600

    site update

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,6 @@ module.exports = {
         md: '768px',
         lg: '1024px',
         xl: '1326px',
-       '2xl': '1536px',
       },
       container: {
         center: true,
@@ -30,8 +29,7 @@ module.exports = {
           sm: '640px',
           md: '768px',
           lg: '1024px',
-          xl: '1326px',
-         '2xl': '1536px',
+          xl: '1326px'
         },
       },
     }
